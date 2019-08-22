5d401b1091c86ebd963b09ab62ab4f32ce122621
Geography410 <54096645+Geography410@users.noreply.github.com>
2019-08-22 17:34:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     a=os.environ['Authorization']
-    return "ภูมิศาสตร์"
+    return a
 
 @app.route("/webhook", methods=['POST'])
 def webhook():
